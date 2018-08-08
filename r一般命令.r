a8111347a5e4dc6bea53bd41cc90b68da81bd10b
jiang_virtual_ubunserver <drjiang3@sina.com>
2018-08-08 08:29:15 +0000

    first modify

--- r一般命令.r
+++ r一般命令.r
@@ -1,7 +1,11 @@
+# 清空工作环境
 rm(list = ls())
-# 判断安装，若不存在某包则安装该包
+
+# 判断安装，若不存在某包则安装该包,显式制定镜像
+# https://mirror.lzu.edu.cn/CRAN/
 if(!require("dplyr")) {
-  install.packages("dplyr", repos = 'https://mirror.lzu.edu.cn/CRAN/',  dependencies = T)
+  install.packages("dplyr",repos = 'https://mirrors.tuna.tsinghua.edu.cn/CRAN/', 
+                   dependencies = T)
 }
 
 # 安装bioconductor
